46c4570cbcc1ede44ccbe5148cced7e401824e02
shakir-shaikh-7 <110457272+shakir-shaikh-7@users.noreply.github.com>
2022-11-27 19:56:42 +0530

    Add files via upload

---
+++ Team-selection.py/db.py
@@ -0,0 +1,68 @@
+db = {
+    'Rohit': {'fname': 'Rohit', 'lname': 'sharma','iplteam': 'MI','department': 'Bat', 'age': '33','run':'4250'},
+    
+     'Ruturaj': {'fname': 'Ruturaj', 'lname': 'Gaikwad','iplteam': 'CSK','department': 'Bat', 'age': '26','run':'1369'},
+
+    'Kohli': {'fname': 'virat', 'lname': 'Kohli','iplteam': 'RCB','department': 'Bat', 'age': '32','run':'5412'},
+
+    'Surya': {'fname': 'Suryakumar', 'lname': 'yadav','iplteam': 'MI', 'department': 'Bat', 'age': '29','run':'3250'},
+
+    'Tyagi': {'fname': 'kartik', 'lname': 'Tyagi',  'iplteam': 'RR','department': 'bowl', 'age': '24','run':'389'},
+
+    'Pant': {'fname': 'Rishabh', 'lname': 'Pant', 'iplteam': 'DD','deptartment': 'Bat', 'age': '26','run':'3257'},
+
+    'Bumrah': {'fname': 'Jasprit', 'lname': 'Bumrah', 'iplteam': 'MI', 'deptartment': 'Bowl','age': '29','run':'478'},
+
+    'Shubhaman': {'fname': 'Shubhaman', 'lname': 'Gill', 'iplteam': 'GT','deptartment': 'Bat', 'age': '25','run':'2748'},
+
+    'jadeja': {'fname': 'ravindra', 'lname': 'jadeja','iplteam': 'CSK', 'deptartment': 'All','age': '33','run':'3491' }
+}
+
+
+student_list=[
+
+    ['fname','sham','lname','yadav','roll_no',1,'std',5,'age',10,'intrest','sport'],
+
+    ['fname','santosh','lname','patil','roll_no',2,'std',7,'age',12,'intrest','reading'],
+
+    ['fname','dhans','lname','kale','roll_no',3,'std',4,'age',9,'intrest','study'],
+
+    ['fname','vishal','lname','pawar','roll_no',4,'std',5,'age',10,'intrest','study'],
+
+    ['fname','kalyan','lname','kakde','roll_no',5,'std',4,'age',9,'intrest','sport'],
+
+    ['fname','samir','lname','pathan','roll_no',6,'std',7,'age',12,'intrest','reading'],
+
+    ['fname','kunal','lname','more','roll_no',7,'std',5,'age',10,'intrest','sport'],
+
+    ['fname','karan','lname','kakde','roll_no',8,'std',4,'age',9,'intrest','sport']
+    
+]
+
+def student_table_format():
+    print(f"{'-'*125}")
+    print(f'\n{"Sr_no"}\t{"First_Name"}\t\t{"Last_Name"} \t{"Roll_name"}\t{"STD"}\t\t{"Age"}\t\t{"Intrest"}')
+    print(f"{'-'*125}")
+
+
+employee_dict={
+
+
+    1:{'emp_id':1,'fname':'sham','lname':'yadav','salary':11250,'age':27,'dept':'sales'},
+
+    2:{'emp_id':2,'fname':'santosh','lname':'patil','salary':12540,'age':32,'dept':'it'},
+
+    3:{'emp_id':3,'fname':'dhans','lname':'kale','salary':11250,'age':24,'dept':'hr'},
+
+    4:{'emp_id':4,'fname':'vishal','lname':'pawar','salary':12540,'age':35,'dept':'it'},
+
+    5:{'emp_id':5,'fname':'kalyan','lname':'kakde','salary':11250,'age':24,'dept':'hr'},
+
+    6:{'emp_id':6,'fname':'samir','lname':'pathan','salary':12540,'age':32,'dept':'it'},
+
+    7:{'emp_id':7,'fname':'kunal','lname':'more','salary':10500,'age':30,'dept':'hr'},
+
+    # 8:{'emp_id':8,'fname':'karan','lname':'kakde','salary':10500,'age':28,'dept':'sales'}:
+
+
+}
